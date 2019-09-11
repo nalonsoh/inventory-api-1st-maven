c3da86476978f6e1fe9543cc17c7d924c4b0a8fd
Noel Alonso Hernández <noelah@ext.inditex.com>
2019-09-11 12:35:16 +0200

    Prometheus metrics on

--- src/main/java/com/redhat/cloudnative/inventory/api/InventoryApi.java
+++ src/main/java/com/redhat/cloudnative/inventory/api/InventoryApi.java
@@ -5,28 +5,27 @@
  */
 package com.redhat.cloudnative.inventory.api;
 
-import com.redhat.cloudnative.inventory.model.GenericError;
-import com.redhat.cloudnative.inventory.model.InventoryItem;
-import io.swagger.annotations.*;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
-import javax.validation.constraints.*;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import com.redhat.cloudnative.inventory.model.GenericError;
+import com.redhat.cloudnative.inventory.model.InventoryItem;
+
+import io.micrometer.core.instrument.Metrics;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-09-11T11:15:21.081+02:00[Europe/Belgrade]")
 
 @Validated
@@ -44,6 +43,10 @@ public interface InventoryApi {
         produces = { "application/json" }, 
         method = RequestMethod.GET)
     default ResponseEntity<List<InventoryItem>> inventoryGet() {
+    	// Prometheus metric
+    	  Metrics.counter("api.http.requests.total", "api", "inventory", "method", "GET", "endpoint", 
+    	     "/inventory").increment();
+    	  // <<< Prometheus metric
         getRequest().ifPresent(request -> {
             for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
                 if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
@@ -65,7 +68,11 @@ public interface InventoryApi {
         produces = { "application/json" }, 
         method = RequestMethod.GET)
     default ResponseEntity<InventoryItem> inventoryItemIdGet(@ApiParam(value = "",required=true) @PathVariable("itemId") String itemId) {
-        getRequest().ifPresent(request -> {
+    	 // >>> Prometheus metric
+    	  Metrics.counter("api.http.requests.total", "api", "inventory", "method", "GET", "endpoint", 
+    	     "/inventory/" + itemId).increment();
+    	  // <<< Prometheus metric
+    	getRequest().ifPresent(request -> {
             for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
                 if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
                     ApiUtil.setExampleResponse(request, "application/json", "{  \"itemId\" : \"329299\",  \"quantity\" : 35}");
